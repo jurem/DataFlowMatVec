5ae467d6b8d328e62a3b95e657236b49f8f432a8
Jure Mihelič <jure.mihelic@fri.uni-lj.si>
2016-07-17 19:28:31 -0700

    Added project Col

---
+++ Col/CPUCode/ColCpuCode.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <getopt.h>
+
+#include <MaxSLiCInterface.h>
+#include "Maxfiles.h"
+
+#include "common/common.h"
+#include "common/timer.h"
+#include "common/matrix.h"
+#include "common/matvec.h"
+
+
+int main(int argc, char * argv[]) {
+	INIT_MATVEC;
+
+	mat_t matAT = mat_make(size, size);
+	mat_transpose(size, size, matA, matAT);
+	MatVecMultiply(size, matAT, vecB, vecC);
+	free(matAT);
+
+	DONE_MATVEC;
+}
